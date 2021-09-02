87ce536bd659d60c720d2f3385a1b366f0544c92
AJ <aj@smartnvm.com>
2021-09-02 07:42:50 +0000

    initial commit with all my work so far

---
+++ array-filtering.js
@@ -0,0 +1,18 @@
+var numbers = [1,2,3,4,5,6,7,8,9,10]
+
+const filtered = numbers.filter(
+  function(num){
+  return (num % 2 === 0)}
+  )
+
+console.log(filtered)
+
+/*
+const numbersx = [1,2,3,4,5,6,7,8,9,10]  
+       
+const filteredx = numbersx.filter(function (numx) {  
+       return (numx % 2 === 0)})
+
+
+console.log(filteredx)
+*/
---
+++ array.js
@@ -0,0 +1,5 @@
+const pet = ['cat','dog', 'rat']
+
+var pizzaToppings = ['tomato sauce','cheese','pepperoni']
+
+console.log(pizzaToppings)
